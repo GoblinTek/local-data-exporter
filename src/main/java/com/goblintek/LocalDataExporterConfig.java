78063fcaba236614f416fb2787e6959f2858b708
GoblinTek <282744325+GoblinTek@users.noreply.github.com>
2026-05-18 06:27:28 +0200

    Update LocalDataExporterConfig.java

--- src/main/java/com/goblintek/LocalDataExporterConfig.java
+++ src/main/java/com/goblintek/LocalDataExporterConfig.java
@@ -8,16 +8,6 @@ import net.runelite.client.config.ConfigItem;
 public interface LocalDataExporterConfig extends Config
 {
 	@ConfigItem(
-			keyName = "exportDirectory",
-			name = "Export Directory",
-			description = "Directory where JSON snapshot files are written. Leave empty to use the default RuneLite folder."
-	)
-	default String exportDirectory()
-	{
-		return "";
-	}
-
-	@ConfigItem(
 			keyName = "exportIntervalTicks",
 			name = "Export Interval",
 			description = "Number of game ticks between snapshot exports. Lower values export more often."
